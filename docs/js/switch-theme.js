0b999b4a3e4c51f2e65d4f123534bba039ed3c85
Junaid <sj.shaikhjunaid@gmail.com>
2022-02-03 12:34:15 +0530

    persist night node on refresh

--- docs/js/switch-theme.js
+++ docs/js/switch-theme.js
@@ -1,12 +1,25 @@
 const switchBtn = document.querySelector(".theme-switch-btn");
 
-switchBtn.addEventListener("click", () => {
-  console.log("clicked");
-  document.body.classList.toggle("dark-mode");
+let darkMode = false;
 
-  if (document.body.classList.contains("dark-mode")) {
-    switchBtn.innerHTML = '<i class="fas fa-sun" title="switch theme"></i>';
-  } else {
+if (localStorage.getItem("theme")) {
+  document.body.classList.add("dark-mode");
+  switchBtn.innerHTML = '<i class="fas fa-sun" title="switch theme"></i>';
+  darkMode = true;
+}
+
+switchBtn.addEventListener("click", () => {
+  if (darkMode) {
+    document.body.classList.remove("dark-mode");
+    localStorage.removeItem("theme");
     switchBtn.innerHTML = '<i class="fas fa-moon" title="switch theme"></i>';
+
+    darkMode = false;
+  } else {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+    switchBtn.innerHTML = '<i class="fas fa-sun" title="switch theme"></i>';
+
+    darkMode = true;
   }
 });
